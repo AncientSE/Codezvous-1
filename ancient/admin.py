398e494ca4b5d4a13aec50f1ef94e15dda8a43eb
ricky <ricky@rickyim.com>
2018-01-27 15:10:41 +0800

    merge 20180127

--- ancient/admin.py
+++ ancient/admin.py
@@ -1,6 +1,10 @@
 
 from django.contrib import admin
-from .models import Submit, Identity, ClassTable, ClassChoose, Homework
+from .models import Submit, Identity, ClassTable, ClassChoose, Homework,newuser
+@admin.register(newuser)
+class newuserAdmin(admin.ModelAdmin):
+    list_display = ('id','username','email','password')
+    ordering = ('id',)
 
 admin.site.register(Submit)
 admin.site.register(Identity)
